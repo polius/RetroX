ef3a9dffcbf91b40dbe904223c9c609470f22bd8
Pol Alzina <palzina@Pols-MacBook-Pro.local>
2024-04-02 19:12:16 +0200

    Added more code

--- js/scripts/register.js
+++ js/scripts/register.js
@@ -1,11 +1,32 @@
-function turnstileCallback(token) {
+function turnstileCallback() {
   const submitButton = document.getElementById("submit");
-  // submitButton.setAttribute("disabled", "");
   submitButton.removeAttribute("disabled");
 }
 
-const register = () => {
-  // Get data
+function showAlert(type, message) {
+  const registerAlert = document.getElementById('registerAlert')
+  registerAlert.innerHTML = `
+    <div class="alert alert-${type} alert-dismissible" role="alert">
+      <div style="text-align:left">
+        ${type == 'success'
+        ? 
+          `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16" style="margin-bottom:3px; margin-right:3px">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+          </svg>`
+        :
+          `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16" style="margin-bottom:3px; margin-right:3px">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+          </svg>`
+        }
+        ${message}
+      </div>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+  `
+}
+
+async function register() {
+  // Get elements
   const email = document.getElementById("email");
   const username = document.getElementById("username");
   const password = document.getElementById("password");
@@ -13,74 +34,75 @@ const register = () => {
   const registerModal = new bootstrap.Modal(document.getElementById('registerModal'));
   const registerModalError = document.getElementById("registerModalError");
   const cfToken = turnstile.getResponse();
-
-  // Check if token is valid
-  // if (cf_token === undefined) {
-  //   registerModalError.innerText = "Confirm the Cloudflare Captcha."
-  //   registerModal.show();
-  // }
+  const submitButton = document.getElementById("submit");
+  const registerForm = document.getElementById("registerForm");
+  const resendDiv = document.getElementById("resendDiv");
 
   // Check if all values are filled
   if (email.value.length == 0 || username.value.length == 0 || password.value.length == 0 || repeatPassword.value.length == 0) {
-    registerModalError.innerText = "Please fill all fields."
-    registerModal.show();
+    showAlert("warning", "Please fill out all fields.")
+    // registerModalError.innerText = "Please fill out all fields."
+    // registerModal.show();
+    return
+  }
+
+  // Check if email is valid
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email.value)) {
+    showAlert("warning", "Please enter a valid email.")
+    return
   }
 
   // Check if the passwords match
   if (password.value != repeatPassword.value) {
-    registerModalError.innerText = "The two passwords does not match. Please enter again the password."
+    showAlert("warning", "The two passwords do not match.")
     password.value = ''
     repeatPassword.value = ''
-    registerModal.show();
+    password.focus()
+    return
   }
 
-  fetch("https://api.retrox.app/register", {
-    method: "POST",
-    body: JSON.stringify({
-      email: email,
-      username: username,
-      password: password,
-    }),
-  })
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('network returns error');
+  // Disable the submit button
+  submitButton.setAttribute("disabled", "");
+
+  // Perform the Register request
+  try {
+    const response = await fetch("https://api.retrox.app/register", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        username: username,
+        password: password,
+        token: cfToken,
+      })
+    })
+
+    const json = await response.json()
+    if (!response.ok) showAlert("danger", json['message'])
+    else {
+      showAlert("success", json['message'])
+      registerForm.style.display = 'none'
+      resendDiv.style.display = 'flex'
+      turnstile.execute('#turnstileConfirm')
+      // turnstile.execute('#turnstileConfirm', {
+      //   callback: function (token) {
+      //     console.log(`Challenge Success ${token}`);
+      //   }
+      // })
     }
-    return response.json();
-  })
-  .then((response) => {
-
-  })
-  .catch((error) => {
-      // Handle error
-      console.log("error ", error);
-  });
-
-  console.log("hi")
-  const token = turnstile.getResponse()
-  console.log(token)
-  if (token === undefined) console.log("KO!")
+  }
+  catch (error) {
+    showAlert("danger", "An error occurred. Please try again.")
+  }
+  finally {
+    submitButton.removeAttribute("disabled");
+  }
 }
 
-// const register = (email, username, password) => {
-//     return new Promise((resolve, reject) => {
-//         fetch(`${apiGatewayUrl}/register/`, {
-//             method: 'POST',
-//             credentials: 'include',
-//             body: JSON.stringify({"email": email, "username": username, "password": password})
-//         })
-//         .then((response) => {
-//             if (response.ok) return response.json().then(json => {
-//                 localStorage.setItem('auth', JSON.stringify({"username": json['username'], "expires": json['expires']}))
-//                 resolve()
-//             })
-//             response.json().then(json => reject({"status": response.status, "message": json['message']}));
-//         })
-//         .catch(() => {
-//             reject({"status": 429, "message": "Too Many Requests"})
-//         })
-//     })
-// }
-
-
-// await apigateway.login(username, password);
+function resend() {
+  turnstile.execute('#turnstileConfirm', {
+    callback: function (token) {
+      console.log(`Challenge Success ${token}`);
+    }
+  })
+}
---
+++ js/scripts/verify.js
@@ -0,0 +1,57 @@
+function showAlert(type, message) {
+  const verifyAlert = document.getElementById('verifyAlert')
+  verifyAlert.innerHTML = `
+    <div class="alert alert-${type} alert-dismissible" role="alert">
+      <div style="text-align:left">
+        ${type == 'success'
+        ? 
+          `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16" style="margin-bottom:3px; margin-right:3px">
+            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+          </svg>`
+        :
+          `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16" style="margin-bottom:3px; margin-right:3px">
+            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+          </svg>`
+        }
+        ${message}
+      </div>
+    </div>
+  `
+}
+
+async function verify() {
+  // Get elements
+  const startButton = document.getElementById('startButton');
+
+  // Get URL Parameters
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  
+  // Check 'verify_id' parameter
+  if (urlParams.get('verify_id').length == 0) {
+    showAlert("danger", "This URL is not valid.")
+    return
+  }
+
+  // Perform the verify request
+  try {
+    const response = await fetch("https://api.retrox.app/verify", {
+      method: "POST",
+      body: JSON.stringify({
+        verify_id: urlParams.ger('verify_id')
+      })
+    })
+
+    const json = await response.json()
+    if (!response.ok) showAlert("danger", json['message'])
+    else {
+      showAlert("success", json['message'])
+      startButton.style.display = 'block'
+    }
+  }
+  catch (error) {
+    showAlert("danger", "An error occurred. Please try again.")
+  }
+}
+
+showAlert("success", "Account validated. Welcome!")
